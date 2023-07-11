8c3b07353d2f074a635b04009ddb388fe1ef5ebe
Sebastián Chavarría <schavarria13@alumnos.uaq.mx>
2023-07-10 23:55:34 -0600

    Curso Typescript Udemy - Bases

---
+++ bases/tipos/array.js
@@ -0,0 +1,6 @@
+"use strict";
+(() => {
+    const numbers = [1, 2, 3, 4, 6, 7, 8];
+    numbers.push(11);
+    console.log(numbers);
+})();
---
+++ bases/tipos/array.ts
@@ -0,0 +1,8 @@
+(()=>{
+    const numbers: number[] = [1,2,3,4,6,7,8]
+
+    numbers.push(11);
+
+    console.log(numbers)
+ 
+})();
